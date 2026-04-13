cf23a6333d278ead9be4e02e820e02fdfc37b056
123rafiuolajumoke@gmail.com <123rafiuolajumoke@gmail.com>
2026-04-13 20:31:18 +0100

    Fix CORS, CSRF and database settings for production

--- travel/settings.py
+++ travel/settings.py
@@ -96,11 +96,10 @@ ASGI_APPLICATION = 'travel.asgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600,
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
