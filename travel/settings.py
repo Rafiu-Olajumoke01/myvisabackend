f0a5a552872badc77ee0eb95ced9a3e2699b05c4
123rafiuolajumoke@gmail.com <123rafiuolajumoke@gmail.com>
2026-04-13 15:19:12 +0100

    fix allowed hosts

--- travel/settings.py
+++ travel/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&ap^^6-dk0e=pp)d8jqfj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
