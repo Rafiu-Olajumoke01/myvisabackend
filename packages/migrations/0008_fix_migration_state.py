72f2ac37ba7a8b63f79bbb668d21f67fb9b30917
123rafiuolajumoke@gmail.com <123rafiuolajumoke@gmail.com>
2026-04-21 14:01:44 +0100

    fix: add 0008 migration to fix bad 0007 state on production

---
+++ packages/migrations/0008_fix_migration_state.py
@@ -0,0 +1,16 @@
+﻿from django.db import migrations
+
+def fix_migration_state(apps, schema_editor):
+    schema_editor.execute(
+        "DELETE FROM django_migrations WHERE app = 'packages' AND name = '0007_alter_package_service_id';"
+    )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('packages', '0006_5_backfill_service_id'),
+    ]
+
+    operations = [
+        migrations.RunPython(fix_migration_state, migrations.RunPython.noop),
+    ]
